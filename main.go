0dc515172150f7b61cca8bae7957a2d2fff0acc5
Uuganbayar <168924684+uux4na@users.noreply.github.com>
2024-05-06 19:59:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -27,8 +28,37 @@ func setupRouter(client *firestore.Client) *gin.Engine {
 		c.String(http.StatusOK, "pong")
 	})
 
-	r.GET("/urlcheck/:url", func(c *gin.Context) {
+	r.POST("/urlcheck", func(c *gin.Context) {
+		var requestBody map[string]string
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		url, exists := requestBody["url"]
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "URL is required"})
+			return
+		}
 
+		iter := client.Collection("bot-profiles").Documents(ctx)
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Printf("Error querying Firestore: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
+			firestoreURL := doc.Data()["url"].(string)
+			if url == firestoreURL {
+				c.JSON(http.StatusOK, gin.H{"valid": true, "message": "BOT found"})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"valid": false, "message": "BOT not found"})
 	})
 
 	r.POST("/addprofile", func(c *gin.Context) {
@@ -52,7 +82,7 @@ func setupRouter(client *firestore.Client) *gin.Engine {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{"user": user})
+		c.JSON(http.StatusOK, gin.H{"User successfully added": user})
 	})
 
 	return r
